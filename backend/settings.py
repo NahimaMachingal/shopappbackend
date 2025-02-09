79c3cb359320fe6907458469eb83f376bb25a7f2
Nahima Machingal <mhnahima@gmail.com>
2025-02-09 17:17:07 +0400

    settings updated

--- backend/settings.py
+++ backend/settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Example: React frontend
+    "https://shopappfrontend.vercel.app",
+    "https://shopappbackend-w541.onrender.com",
     
 ]
 
